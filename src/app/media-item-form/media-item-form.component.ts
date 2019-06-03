24e65b743c044945c3f80d2c0fbe54d64262c516
PirunSeng <sengpirun.rain@gmail.com>
2019-06-03 17:30:59 +0700

    Completes 4. Forms

---
+++ src/app/media-item-form/media-item-form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-media-item-form',
+  templateUrl: './media-item-form.component.html',
+  styleUrls: ['./media-item-form.component.css']
+})
+export class MediaItemFormComponent implements OnInit {
+  form;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      medium: new FormControl('Movies'),
+      // name: new FormControl('', Validators.pattern('[\\w\\-\\s\\/]+')),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
+      category: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
+    });
+  }
+
+  yearValidator(control) {
+    if(control.value.trim().length === 0) {
+      return null;
+    }
+    let year = parseInt(control.value);
+    let minYear = 1800;
+    let maxYear = 2500;
+    if(year >= minYear && year <= maxYear) {
+      return null;
+    }else {
+      return {'year': {
+        'min': minYear,
+        'max': maxYear
+      }};
+    }
+  }
+
+  onSubmit(mediaItem){
+    console.log(mediaItem);
+  }
+
+}
